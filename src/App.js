933553e93142348b54da9b326fbb7ca4a2d8b159
Tekkadan96 <yerryrizal@gmail.com>
2022-12-23 06:04:32 +0700

    adding feature 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Saya bisa !
+          Feature-2 terupload !
         </p>
         <a
           className="App-link"
